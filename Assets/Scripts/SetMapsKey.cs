ac9de0ddc4b3518452a07699f760994e1b50ed3c
Aysegul Yonet <aysegulyonet@gmail.com>
2020-01-23 23:40:55 -0800

    scene(cityTour): add key and tour script

---
+++ Assets/Scripts/SetMapsKey.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maps.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMapsKey : MonoBehaviour
+{
+    private GameObject map;
+    private MapRenderer mapRenderer;
+    private string bingMapsKey;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        map = GameObject.Find("Map");
+        mapRenderer = map.GetComponent<MapRenderer>();
+        bingMapsKey = Environment.GetEnvironmentVariable("BingMapsKey");
+        mapRenderer.BingMapsKey = bingMapsKey;
+    }
+}
